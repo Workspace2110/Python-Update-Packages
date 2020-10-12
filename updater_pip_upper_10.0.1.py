21fb67f57b6a5692f5e965a3892244aa77b04c72
Jheng-Hong Wang <workspace2110@gmail.com>
2020-10-12 13:58:08 +0800

    Normalize Python file

--- updater_pip_upper_10.0.1.py
+++ updater_pip_upper_10.0.1.py
@@ -1,23 +1,31 @@
 """
-Warning: It will override dependency packages to the latest version.
+Warning:
+It will override dependency packages to the latest version.
 If you want to easy control dependency packages version, please use poetry to control version.
 """
 
+# import packages
 import pkg_resources
 from subprocess import call
 
 
-def main():
+def update_with_pip():
     print("Warning: It will override dependency packages to the latest version. \
         If you want to easy control dependency packages version, please use poetry to control version.")
+
     print("Start updating packages")
+
+    # load all packages names
     packages = [dist.project_name for dist in pkg_resources.working_set]
+
+    # Do command: Update packages with pip
     try:
         call("pip install --upgrade " + ' '.join(packages), shell=True)
     except Exception as e:
         print("Error: {error}".format(error=e))
+
     print("Update finish")
 
 
 if __name__ == "__main__":
-    main()
+    update_with_pip()
